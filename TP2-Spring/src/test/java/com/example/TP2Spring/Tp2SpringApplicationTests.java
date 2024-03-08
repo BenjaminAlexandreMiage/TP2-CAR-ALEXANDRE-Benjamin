3d7ff77f4126b85cec455c4fc244872deedbe698
benjamin alexandre <benjamin.alexandre.etu@univ-lille.fr>
2024-03-08 08:26:20 +0100

    Mise en place du projet spring

---
+++ TP2-Spring/src/test/java/com/example/TP2Spring/Tp2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
