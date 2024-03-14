6ed8649f3bb3ede4fc8876453ff3e4dffcf6bc84
benjamin alexandre <benjamin.alexandre.etu@univ-lille.fr>
2024-03-14 13:48:20 +0100

    Avancement de l'iteration 2 mais blocage au niveau de l affichage des agendas

--- TP2-Spring/src/main/java/com/example/TP2Spring/agenda/AgendaRepository.java
+++ TP2-Spring/src/main/java/com/example/TP2Spring/agenda/AgendaRepository.java
@@ -1,7 +1,10 @@
 package com.example.TP2Spring.agenda;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AgendaRepository extends CrudRepository<Agenda, Long> {
+	
+	public Iterable <Personne> findByIdProprietaire( @Param("idProprietaire") Long idProprietaire);
 	
 }
--- TP2-Spring/src/main/java/com/example/TP2Spring/agenda/PersonneController.java
+++ TP2-Spring/src/main/java/com/example/TP2Spring/agenda/PersonneController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -43,26 +44,39 @@ public class PersonneController {
 		session.setAttribute("nom", verifPersonne.getNom());
 		session.setAttribute("prenom", verifPersonne.getPrenom());
 		session.setAttribute("mail", verifPersonne.getMail());
+		session.setAttribute("mdp", verifPersonne.getMdp());
 		session.setAttribute("id", verifPersonne.getId());
 		
 		return "agenda/connexion";
 	}
 	
+	@GetMapping("/connexion")
+	public String Connexion() {
+		return "agenda/connexion";
+	}
+	
+	
 	@GetMapping("/deconnexion")
 	public String Deconnexion(HttpSession session) {
 		
-		session.setAttribute("nom",null);
-		session.setAttribute("prenom",null);
-		session.setAttribute("mail",null);
-		session.setAttribute("id", null);
+		session.removeAttribute("nom");
+		session.removeAttribute("prenom");
+		session.removeAttribute("mail");
+		session.removeAttribute("id");
+		session.removeAttribute("mdp");
 		
 		return "redirect:/agenda/home";
 	}
 	
 	
 	@PostMapping("/addAgenda")
-	public String add (@RequestParam String titre, @RequestParam Long idProprietaire) {
-		serviceAgenda.ajouterAgenda(titre,idProprietaire);
+	public String add (@RequestParam String titre,@RequestParam String mail,@RequestParam String mdp,Model model) {
+		Personne proprio = service.findPersonne(mail, mdp);
+		serviceAgenda.ajouterAgenda(titre,proprio.getId());
+		
+		Iterable<Agenda> agendas = serviceAgenda.getAllAgenda();
+		model.addAttribute("agendas", agendas);
+	
 		return "redirect:/agenda/connexion";
 	}
 	
