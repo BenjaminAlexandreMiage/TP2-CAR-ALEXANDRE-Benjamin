0e24a98e83b356750e6ce10069dc49052cd4446f
benjamin alexandre <benjamin.alexandre.etu@univ-lille.fr>
2024-03-22 09:00:19 +0100

    ajout de l iteration 5

--- TP2-Spring/src/main/java/com/example/TP2Spring/agenda/PersonneController.java
+++ TP2-Spring/src/main/java/com/example/TP2Spring/agenda/PersonneController.java
@@ -30,17 +30,20 @@ public class PersonneController {
 	@Autowired
 	private EvenementService serviceEvenement;
 	
+	//Page d'accueil
 	@GetMapping("/home")
 	public String home() {
 		return "/agenda/home";
 	}
 	
+	//Ajouter un compte 
 	@PostMapping("/add")
 	public String add (@RequestParam String mail, @RequestParam String mdp, @RequestParam String nom, @RequestParam String prenom  ) {
 		service.ajouterPersonne(mail,mdp,nom,prenom);
 		return "redirect:/agenda/home";
 	}
 	
+	//Ce connecte via la page de login
 	@PostMapping("/connexion")
 	public String Connexion(@RequestParam String mail,@RequestParam String mdp,HttpSession session,Model model) {
 		Personne verifPersonne = service.findPersonne(mail,mdp);
@@ -60,28 +63,40 @@ public class PersonneController {
 		return "agenda/connexion";
 	}
 	
+	//Accès à la page utilisateur en get
 	@GetMapping("/connexion")
 	public String Connexion(Model model,HttpSession session) {
+		
+		String mail = (String) session.getAttribute("nom");
+		
+		if(mail==null) {
+			return "redirect:/agenda/home";
+		}
+		
+			
 		Iterable<Agenda> agendas = serviceAgenda.getAgendaByIdProprietaire((Long) session.getAttribute("id"));
 		model.addAttribute("agendas", agendas);
-		
+			
 		return "agenda/connexion";
+		
+		
 	}
 	
-	
+	//Gère la deconnexion (et la suppression de la session
 	@GetMapping("/deconnexion")
 	public String Deconnexion(HttpSession session) {
 		
 		session.removeAttribute("nom");
 		session.removeAttribute("prenom");
 		session.removeAttribute("mail");
-//		session.removeAttribute("id");
+		session.removeAttribute("id");
 		session.removeAttribute("mdp");
 		
 		return "redirect:/agenda/home";
 	}
 	
 	
+	//Ajout d'un agenda
 	@PostMapping("/addAgenda")
 	public String add (@RequestParam String titre,@RequestParam String mail,@RequestParam String mdp) {
 		Personne proprio = service.findPersonne(mail, mdp);
@@ -90,6 +105,7 @@ public class PersonneController {
 		return "redirect:/agenda/connexion";
 	}
 	
+	//Accès à la page de l'agenda
 	@PostMapping("/directionAgenda")
 	public String directionAgenda (@RequestParam Long idAgenda,@RequestParam Long idProprietaire,Model model,HttpSession session) {
 		Agenda verifAgenda = serviceAgenda.findAgenda(idAgenda);
@@ -107,16 +123,24 @@ public class PersonneController {
 		return "agenda/pageAgenda";
 	}
 	
-	
+	//Accès à la page de l'agenda en get 
 	@GetMapping("/directionAgenda")
 	public String directionAgenda(Model model,HttpSession session) {
+		
+		String mail = (String) session.getAttribute("nom");
+		
+		if(mail==null) {
+			return "redirect:/agenda/home";
+		}
+			
 		Iterable<Evenement> evenements = serviceEvenement.getEvenementOfAgenda((Long) session.getAttribute("idAgenda"));
 		model.addAttribute("evenements", evenements);
-		
+			
 		return "agenda/pageAgenda";
+		
 	}
 	
-	
+	//Ajout d'un évenement
 	@PostMapping("/addEvenement")
 	public String addEvenement (@RequestParam String titre,@RequestParam String date,@RequestParam Long idAgenda) throws ParseException {
 		
@@ -129,8 +153,15 @@ public class PersonneController {
 		return "redirect:/agenda/directionAgenda";
 	}
 	
+	//Quitté la page de l'agenda
 	@GetMapping("/quitteAgenda")
 	public String quitteAgenda(HttpSession session) {
+		
+		String mail = (String) session.getAttribute("nom");
+		
+		if(mail==null) {
+			return "redirect:/agenda/home";
+		}
 		
 		session.removeAttribute("idProprietaire");
 		session.removeAttribute("idAgenda");
@@ -139,6 +170,7 @@ public class PersonneController {
 		return "redirect:/agenda/connexion";
 	}
 	
+	//Supprimer un évenement
 	@PostMapping("/supprimerEvenement")
 	public String supprimerEvenement(@RequestParam Long idEvenement) {
 		
@@ -147,6 +179,7 @@ public class PersonneController {
 		return "redirect:/agenda/directionAgenda";
 	}
 	
+	//Affiche l'impression d'un agenda
 	@PostMapping("/afficheImpression")
 	public String afficheImpression(Model model,HttpSession session) {
 		
